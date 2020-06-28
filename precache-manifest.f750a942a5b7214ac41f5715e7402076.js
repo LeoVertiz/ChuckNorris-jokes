07c9f4cf363aa92a4a4f32fe86fb4e22e1346487
Leonardo Vertiz <leovertiz@outlook.com>
2020-06-28 07:43:54 -0500

    Updates

---
+++ precache-manifest.f750a942a5b7214ac41f5715e7402076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c8ca0083ac7ead3f499e14cee1d77d",
+    "url": "/ChuckNorris-jokes/index.html"
+  },
+  {
+    "revision": "ae118c750f3f0f14cbd5",
+    "url": "/ChuckNorris-jokes/static/css/main.3a6d51ca.chunk.css"
+  },
+  {
+    "revision": "1d724b59e981436a550c",
+    "url": "/ChuckNorris-jokes/static/js/2.753afce6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChuckNorris-jokes/static/js/2.753afce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae118c750f3f0f14cbd5",
+    "url": "/ChuckNorris-jokes/static/js/main.25b0c8b2.chunk.js"
+  },
+  {
+    "revision": "87c9eeea31af89cf027f",
+    "url": "/ChuckNorris-jokes/static/js/runtime-main.2e23f450.js"
+  }
+]);
